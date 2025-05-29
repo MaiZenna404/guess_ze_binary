ad1af9a302c76afb120d23a3c3748ba316f09a01
mai <mai.than.222@gmail.com>
2025-05-29 14:50:55 +0200

    add: added Tailwind, Shadcn and dark Theme

---
+++ app/Components/BinaryInput.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+/* Import UI components */
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { Button } from "@/components/ui/button";  
+
+
+export default function BinaryInput() {
+    return (
+      <>
+        <InputOTP maxLength={6}>
+          <InputOTPGroup>
+            <InputOTPSlot index={0} />
+            <InputOTPSlot index={1} />
+            <InputOTPSlot index={2} />
+          </InputOTPGroup>
+          <InputOTPSeparator />
+          <InputOTPGroup>
+            <InputOTPSlot index={3} />
+            <InputOTPSlot index={4} />
+            <InputOTPSlot index={5} />
+          </InputOTPGroup>
+            </InputOTP>
+        <Button className="mt-4">Submit</Button>
+      </>
+    );
+}
