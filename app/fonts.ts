48b1db5974819ee06b9a9f084d34e7986ffdb09d
mai <mai.than.222@gmail.com>
2025-06-12 14:46:50 +0200

    fix: fixed the font after deployment on Vercel

---
+++ app/fonts.ts
@@ -0,0 +1,8 @@
+import { Press_Start_2P } from "next/font/google";
+
+export const pixelFont = Press_Start_2P({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-pixels",
+});
